afcc725da2220f7a4a81898e39793167f6c9f6b0
artisters <you_leet@foxmail.com>
2018-02-05 18:23:45 +0800

    #init project

--- prototype-admin/src/main/java/org/ko/prototype/admin/controller/AdminController.java
+++ prototype-admin/src/main/java/org/ko/prototype/admin/controller/AdminController.java
@@ -1,16 +1,27 @@
 package org.ko.prototype.admin.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiParam;
+import org.ko.prototype.support.view.View;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Api(value = "AdminController", description = "管理接口")
+@RequestMapping("admin")
 public class AdminController {
 
     private static final Logger _LOGGER = LoggerFactory.getLogger(AdminController.class);
 
+    @PostMapping("/login")
+    public String login (@ApiParam("账户名") @RequestParam("username") String username,
+                               @ApiParam("密码") @RequestParam("password") String password) {
+        return "SUCCESS";
+    }
 
 
 }
