bfe641653c80a9ea29a006d53bac9744f24266e5
Artister <you_leet@foxmail.com>
2018-02-23 22:06:15 +0800

    #draft

--- prototype-admin/src/main/java/org/ko/prototype/admin/PrototypeAdminServer.java
+++ prototype-admin/src/main/java/org/ko/prototype/admin/PrototypeAdminServer.java
@@ -2,16 +2,6 @@ package org.ko.prototype.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.SpringCloudApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.cloud.netflix.feign.EnableFeignClients;
-
-
-//开启Feign客户端
-//@EnableFeignClients
-////向Eureka注册中心发布服务
-//@EnableEurekaClient
-//@SpringCloudApplication
 
 @SpringBootApplication
 public class PrototypeAdminServer {
