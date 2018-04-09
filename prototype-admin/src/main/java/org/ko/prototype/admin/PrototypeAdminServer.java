4efd5bc78086f03d2e4063e5cb240b9a9a85f61c
artisters <you_leet@foxmail.com>
2018-04-09 18:57:59 +0800

    #init project

--- prototype-admin/src/main/java/org/ko/prototype/admin/PrototypeAdminServer.java
+++ prototype-admin/src/main/java/org/ko/prototype/admin/PrototypeAdminServer.java
@@ -1,10 +1,15 @@
 package org.ko.prototype.admin;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@SpringBootApplication
+//开启Feign客户端
+@EnableFeignClients
+//向Eureka注册中心发布服务
+@EnableEurekaClient
+@SpringCloudApplication
 public class PrototypeAdminServer {
 
     public static void main(String[] args) {
