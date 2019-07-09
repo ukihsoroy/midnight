566d000a9313bf6d121b33cb0127da5720a65949
uzies <ko.shen@hotmail.com>
2019-07-09 22:46:06 +0800

    #init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# quick-sigma-micorservice
+# sigma-bigdata
+
+- _dist: 样例工程
+- insight: 元数据管理与模型提交平台
+- platform-core: 基础依赖包
---
+++ _dist/src/main/scala/org/ko/App.scala
@@ -0,0 +1,9 @@
+package org.ko
+
+/**
+ * Hello world!
+ *
+ */
+object App extends Application {
+  println( "Hello World!" )
+}
