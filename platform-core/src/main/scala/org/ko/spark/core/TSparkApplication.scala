08a335fe3ec61ece9821831e4bcab2949b8022a7
uzies <ko.shen@hotmail.com>
2019-07-10 11:26:34 +0800

    #添加spark模型编程模板

---
+++ platform-core/src/main/scala/org/ko/spark/core/TSparkApplication.scala
@@ -0,0 +1,26 @@
+package org.ko.spark.core
+
+trait TSparkApplication extends Serializable {
+
+  /**
+    * 前置
+    */
+  def before()
+
+  /**
+    * 业务逻辑处理
+    */
+  def process()
+
+  /**
+    * 异常处理
+    * @param exp 异常信息
+    */
+  def exception(exp: Throwable)
+
+  /**
+    * 后置
+    */
+  def after()
+
+}
---
+++ platform-core/src/main/scala/org/ko/spark/core/sql/ASparkSQLApplication.scala
@@ -0,0 +1,23 @@
+package org.ko.spark.core.sql
+
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+import org.ko.spark.core.ASparkApplication
+
+abstract class ASparkSQLApplication(args: Array[String]) extends ASparkApplication(args) {
+
+  protected var spark: SparkSession = _
+
+  protected var sc: SparkContext = _
+
+  override def before(): Unit = {
+    spark = SparkSession.builder().config(sparkConf).getOrCreate()
+    sc = spark.sparkContext
+  }
+
+  override def exception(exp: Throwable): Unit = {
+    println("###exp: " + exp.getMessage)
+  }
+
+  override def after(): Unit = spark.stop()
+}
